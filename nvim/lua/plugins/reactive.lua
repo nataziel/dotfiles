c19c14fadeaeee1e75137541fb9861fe8a2d8975
Goose <jack.allan@neds.com>
2025-05-12 22:30:14 +1000

    conform reactive spec to lazy ideal

--- nvim/lua/plugins/reactive.lua
+++ nvim/lua/plugins/reactive.lua
@@ -1,13 +1,4 @@
 return {
     "rasulomaroff/reactive.nvim",
-    config = function()
-        require("reactive").setup({
---             -- builtin = {
---             --     cursorline = true,
---             --     cursor = true,
---             --     modemsg = true,
---             -- },
-            load = { "catppuccin-mocha-cursor", "catppuccin-mocha-cursorline" },
-        })
-    end,
+    opts = { load = { "catppuccin-mocha-cursor", "catppuccin-mocha-cursorline" } },
 }
