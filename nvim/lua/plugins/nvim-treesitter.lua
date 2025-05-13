b06f9581d47deb5d93d4d091f8bac37eef5b56e7
nataziel <aerofuel@gmail.com>
2025-05-13 21:25:05 +1000

    add treesitter text objects

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,25 +1,30 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
     config = function()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
             ensure_installed = {
                 "lua",
+                "vim",
                 "vimdoc",
                 "python",
                 "toml",
                 "yaml",
                 "markdown",
-                "vim",
                 "bash",
                 "go",
                 "sql",
                 "rust",
+                "json",
+                "javascript",
             },
-            sync_install = false,
+            sync_install = true,
+            auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
+            textobjects = { enable = true },
         })
     end,
 }
