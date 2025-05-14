28b8eafabeae1e19f2793e13e2d0cea8da628adf
Goose <jack.allan@neds.com>
2025-05-14 10:29:39 +1000

    add treesitter component to statusline

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,15 +1,27 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		options = {
-			theme = "catppuccin",
-		},
-		extensions = {
-			"neo-tree",
-			"lazy",
-			"mason",
-			"trouble",
-		},
-	},
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        options = {
+            theme = "catppuccin",
+        },
+        extensions = {
+            "neo-tree",
+            "lazy",
+            "mason",
+            "trouble",
+        },
+        sections = {
+            lualine_c = {
+                "filename",
+                function()
+                    return require("nvim-treesitter").statusline({
+                        indicator_size = 70,
+                        type_patterns = { "class", "function", "method" },
+                        separator = "->",
+                    })
+                end,
+            },
+        },
+    },
 }
