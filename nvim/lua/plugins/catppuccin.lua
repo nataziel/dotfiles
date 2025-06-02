46271aed9f8021f4d8ff3cac587ab3f50a5a8c95
Goose <jack.allan@neds.com>
2025-06-02 10:40:22 +1000

    add catppuccin integrations

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -5,14 +5,37 @@ return {
     config = function()
         require("catppuccin").setup({
             integrations = {
+                blink_cmp = true,
                 flash = true,
-                neotree = true,
-                treesitter = true,
+                lsp_trouble = true,
                 mason = true,
+                neotree = true,
+                -- nvim_lsp
+                native_lsp = {
+                    enabled = true,
+                    virtual_text = {
+                        errors = { "italic" },
+                        hints = { "italic" },
+                        warnings = { "italic" },
+                        information = { "italic" },
+                        ok = { "italic" },
+                    },
+                    underlines = {
+                        errors = { "underline" },
+                        hints = { "underline" },
+                        warnings = { "underline" },
+                        information = { "underline" },
+                        ok = { "underline" },
+                    },
+                    inlay_hints = {
+                        background = true,
+                    },
+                },
+                nvim_surround = true,
                 telescope = {
                     enabled = true,
                 },
-                lsp_trouble = true,
+                treesitter = true,
                 which_key = true,
             },
         })
